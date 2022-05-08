bbe8133163a45a423411e6ce5f9512d227d53eb7
SergOswald <skobrusev446@gmail.com>
2022-05-08 21:11:38 +0300

    fix: next version

---
+++ script1.js
@@ -0,0 +1,313 @@
+let div = document.createElement('div');
+div.className = "wripper";
+//textarea.innerHTML = "<strong>Hi there!</strong> You've read an important message.";
+document.body.append( div );
+
+
+let main = document.createElement('main');
+main.className = "main";
+div.append( main );
+
+
+let textarea = document.createElement('textarea');
+textarea.className = "textarea";
+main.append( textarea );
+
+
+let section = document.createElement('section');
+section.className = "keyboard";
+main.append( section );
+
+/*============*/
+
+let section1 = document.createElement('div');
+section1.className = "section1";
+section.append( section1 );
+
+let div1 = document.createElement('div');
+div1.className = "tilda";
+section1.append( div1 );
+
+let div2 = document.createElement('div');
+div2.className = "tilda";
+section1.append( div2 );
+
+let div3 = document.createElement('div');
+div3.className = "tilda";
+section1.append( div3 );
+
+let div4 = document.createElement('div');
+div4.className = "tilda";
+section1.append( div4 );
+
+let div5 = document.createElement('div');
+div5.className = "tilda";
+section1.append( div5 );
+
+let div6 = document.createElement('div');
+div6.className = "tilda";
+section1.append( div6 );
+
+let div7 = document.createElement('div');
+div7.className = "tilda";
+section1.append( div7 );
+
+let div8 = document.createElement('div');
+div8.className = "tilda";
+section1.append( div8 );
+
+let div9 = document.createElement('div');
+div9.className = "tilda";
+section1.append( div9 );
+
+let div10 = document.createElement('div');
+div10.className = "tilda";
+section1.append( div10 );
+
+let div11 = document.createElement('div');
+div11.className = "tilda";
+section1.append( div11 );
+
+let div12 = document.createElement('div');
+div12.className = "tilda";
+section1.append( div12 );
+
+let div13 = document.createElement('div');
+div13.className = "tilda";
+section1.append( div13 );
+
+let div14 = document.createElement('div');
+div14.className = "backspace";
+section1.append( div14 );
+
+/*=======*/
+
+
+let section2 = document.createElement('div');
+section2.className = "section2";
+section.append( section2 );
+
+let div15 = document.createElement('div');
+div15.className = "tab";
+section2.append( div15 );
+
+let div16 = document.createElement('div');
+div16.className = "tilda";
+section2.append( div16 );
+
+let div17 = document.createElement('div');
+div17.className = "tilda";
+section2.append( div17 );
+
+let div18 = document.createElement('div');
+div18.className = "tilda";
+section2.append( div18 );
+
+let div19 = document.createElement('div');
+div19.className = "tilda";
+section2.append( div19 );
+
+let div20 = document.createElement('div');
+div20.className = "tilda";
+section2.append( div20 );
+
+let div21 = document.createElement('div');
+div21.className = "tilda";
+section2.append( div21 );
+
+let div22 = document.createElement('div');
+div22.className = "tilda";
+section2.append( div22 );
+
+let div23 = document.createElement('div');
+div23.className = "tilda";
+section2.append( div23 );
+
+let div24 = document.createElement('div');
+div24.className = "tilda";
+section2.append( div24 );
+
+let div25 = document.createElement('div');
+div25.className = "tilda";
+section2.append( div25 );
+
+let div26 = document.createElement('div');
+div26.className = "tilda";
+section2.append( div26 );
+
+let div27 = document.createElement('div');
+div27.className = "tilda";
+section2.append( div27 );
+
+let div28 = document.createElement('div');
+div28.className = "tilda";
+section2.append( div28 );
+
+let div29 = document.createElement('div');
+div29.className = "del";
+section2.append( div29 );
+
+/*=========*/
+
+let section3 = document.createElement('div');
+section3.className = "section3";
+section.append( section3 );
+
+let div30 = document.createElement('div');
+div30.className = "capslk";
+section3.append( div30 );
+
+let div31 = document.createElement('div');
+div31.className = "tilda";
+section3.append( div31 );
+
+let div32 = document.createElement('div');
+div32.className = "tilda";
+section3.append( div32 );
+
+let div33 = document.createElement('div');
+div33.className = "tilda";
+section3.append( div33 );
+
+let div34 = document.createElement('div');
+div34.className = "tilda";
+section3.append( div34 );
+
+let div35 = document.createElement('div');
+div35.className = "tilda";
+section3.append( div35 );
+
+let div36 = document.createElement('div');
+div36.className = "tilda";
+section3.append( div36 );
+
+let div37 = document.createElement('div');
+div37.className = "tilda";
+section3.append( div37 );
+
+let div38 = document.createElement('div');
+div38.className = "tilda";
+section3.append( div38 );
+
+let div39 = document.createElement('div');
+div39.className = "tilda";
+section3.append( div39 );
+
+let div40 = document.createElement('div');
+div40.className = "tilda";
+section3.append( div40 );
+
+let div41 = document.createElement('div');
+div41.className = "tilda";
+section3.append( div41 );
+
+let div42 = document.createElement('div');
+div42.className = "enter";
+section3.append( div42 );
+
+/*========================*/
+
+let section4 = document.createElement('div');
+section4.className = "section4";
+section.append( section4 );
+
+
+let div43 = document.createElement('div');
+div43.className = "shift";
+section4.append( div43 );
+
+let div44 = document.createElement('div');
+div44.className = "tilda";
+section4.append( div44 );
+
+let div45 = document.createElement('div');
+div45.className = "tilda";
+section4.append( div45 );
+
+let div46 = document.createElement('div');
+div46.className = "tilda";
+section4.append( div46 );
+
+let div47 = document.createElement('div');
+div47.className = "tilda";
+section4.append( div47 );
+
+let div48 = document.createElement('div');
+div48.className = "tilda";
+section4.append( div48 );
+
+let div49 = document.createElement('div');
+div49.className = "tilda";
+section4.append( div49 );
+
+let div50 = document.createElement('div');
+div50.className = "tilda";
+section4.append( div50 );
+
+let div51 = document.createElement('div');
+div51.className = "tilda";
+section4.append( div51 );
+
+let div52 = document.createElement('div');
+div52.className = "tilda";
+section4.append( div52 );
+
+let div53 = document.createElement('div');
+div53.className = "tilda";
+section4.append( div53 );
+
+let div54 = document.createElement('div');
+div54.className = "artop";
+section4.append( div54 );
+
+let div55 = document.createElement('div');
+div55.className = "shift";
+section4.append( div55 );
+
+/*=============*/
+
+let section5 = document.createElement('div');
+section5.className = "section5";
+section.append( section5 );
+
+let div56 = document.createElement('div');
+div56.className = "ctrl";
+section5.append( div56 );
+
+let div57 = document.createElement('div');
+div57.className = "win";
+section5.append( div57 );
+
+let div58 = document.createElement('div');
+div58.className = "alt";
+section5.append( div58 );
+
+let div59 = document.createElement('div');
+div59.className = "prob";
+section5.append( div59 );
+
+let div60 = document.createElement('div');
+div60.className = "tilda";
+section5.append( div60 );
+
+let div61 = document.createElement('div');
+div61.className = "arleft";
+section5.append( div61 );
+
+let div62 = document.createElement('div');
+div62.className = "arbot";
+section5.append( div62 );
+
+let div63 = document.createElement('div');
+div63.className = "arright";
+section5.append( div63 );
+
+let div64 = document.createElement('div');
+div64.className = "ctrl";
+section5.append( div64 );
+
+/*=========*/
+
+
+
+
